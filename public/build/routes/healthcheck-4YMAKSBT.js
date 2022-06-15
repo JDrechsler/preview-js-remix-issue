a32f7f731ff1dc3073bfdc2e01f4af894c66a20b
Johannes Drechsler <JohannesDrechslerUS@gmail.com>
2022-06-15 11:58:50 -0400

    Add files via upload

---
+++ public/build/routes/healthcheck-4YMAKSBT.js
@@ -0,0 +1,14 @@
+import {
+  __commonJS,
+  init_react
+} from "/build/_shared/chunk-6BO74FWO.js";
+
+// browser-route-module:C:\Users\Johannes Drechsler\Documents\tests\remix\remix-tut\blog-tutorial\app\routes\healthcheck.tsx?browser
+var require_healthcheck = __commonJS({
+  "browser-route-module:C:\\Users\\Johannes Drechsler\\Documents\\tests\\remix\\remix-tut\\blog-tutorial\\app\\routes\\healthcheck.tsx?browser"(exports, module) {
+    init_react();
+    module.exports = {};
+  }
+});
+export default require_healthcheck();
+//# sourceMappingURL=/build/routes/healthcheck-4YMAKSBT.js.map
---
+++ public/build/routes/logout-N4ZEU5BG.js
@@ -0,0 +1,14 @@
+import {
+  __commonJS,
+  init_react
+} from "/build/_shared/chunk-6BO74FWO.js";
+
+// browser-route-module:C:\Users\Johannes Drechsler\Documents\tests\remix\remix-tut\blog-tutorial\app\routes\logout.tsx?browser
+var require_logout = __commonJS({
+  "browser-route-module:C:\\Users\\Johannes Drechsler\\Documents\\tests\\remix\\remix-tut\\blog-tutorial\\app\\routes\\logout.tsx?browser"(exports, module) {
+    init_react();
+    module.exports = {};
+  }
+});
+export default require_logout();
+//# sourceMappingURL=/build/routes/logout-N4ZEU5BG.js.map
